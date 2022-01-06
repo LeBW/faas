f7d387c08dff91635c5c9f8507c7641a14e03a12
LeBW <27091925@qq.com>
2022-01-06 09:54:10 +0800

    rename and start cron

--- gateway/handlers/function_scheduler.go
+++ gateway/handlers/function_scheduler.go
@@ -8,11 +8,13 @@ import (
 )
 
 func NewFunctionScheduler(config scaling.ScalingConfig, defaultNamespace string, functionCacher scaling.FunctionCacher) FunctionScheduler {
+	c := cron.New(cron.WithSeconds())
+	c.Start()
 	return FunctionScheduler{
 		Cache:            functionCacher,
 		Config:           config,
 		DefaultNamespace: defaultNamespace,
-		cron:             cron.New(cron.WithSeconds()),
+		cron:             c,
 	}
 }
 
@@ -24,8 +26,8 @@ type FunctionScheduler struct {
 }
 
 func (scheduler *FunctionScheduler) AddPredictions(predictions []Prediction) {
-	len := len(predictions)
-	log.Printf("[AddPredictions] number of predictions: %d\n", len)
+	l := len(predictions)
+	log.Printf("[AddPredictions] number of predictions: %d\n", l)
 	for i := range predictions {
 		scheduler.schedule(predictions[i])
 	}
@@ -37,14 +39,11 @@ func (scheduler *FunctionScheduler) schedule(prediction Prediction) {
 	scheduleCron := time.Unix(0, scheduleTimestamp).Format("05 04 15 02 01 ?")
 	log.Printf("%#v", prediction)
 	log.Printf("[schedule] scheduleTimestamp: %d, cron expression: %s\n", scheduleTimestamp, scheduleCron)
-	_, err := scheduler.cron.AddFunc(scheduleCron, func() {
+	scheduler.cron.AddFunc(scheduleCron, func() {
 		log.Printf("Cron job start. Schedule function %s", prediction.FunctionName)
 		err := scheduler.Config.ServiceQuery.SetReplicas(prediction.FunctionName, scheduler.DefaultNamespace, uint64(prediction.Probability))
 		if err != nil {
 			log.Printf("Schedule function %s Failed, %s\n", prediction.FunctionName, err)
 		}
 	})
-	if err != nil {
-		log.Printf("Add cron func failed! %s\n", err)
-	}
 }
